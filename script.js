76e9d77bba2f1e0b2fb06f4cb4b1c138a5466e8d
Ilaria Colagiorgio <81817242+ilagjo@users.noreply.github.com>
2024-03-16 02:11:27 +0100

    refactored code ITA

--- script.js
+++ script.js
@@ -4,31 +4,21 @@ var contactInfoData = [
     { icon: 'fa-regular fa-envelope', text: 'ilaria.clg@outlook.it', link: 'mailto:ilaria.clg@outlook.it' },
     { icon: 'fa-brands fa-github', text: 'github.com/ilagjo', link: 'http://github.com/ilagjo' },
     { icon: 'fa-brands fa-linkedin', text: 'linkedin.com/ilaria-clg', link: 'http://linkedin.com/ilaria-clg' },
-    { icon: 'fa-solid fa-location-dot', text: 'Milan, Italy', link: 'https://maps.google.com/?q=Milan' }
+    { icon: 'fa-solid fa-location-dot', text: 'Sesto San Giovanni, MI', link: 'https://maps.google.com/?q=Sesto-San-Giovanni' }
 ];
 
 
 function generateContactInfo() {
     var contactList = document.getElementById('contactList');
 
-    contactInfoData.forEach(function (info) {
-        var listItem = document.createElement('li');
-        var iconSpan = document.createElement('span');
-        var textSpan = document.createElement('span');
-        var link = document.createElement('a');
-        
-        iconSpan.classList.add('icon');
-        iconSpan.innerHTML = '<i class="' + info.icon + '"></i>';
-
-        link.textContent = info.text;
-        link.href = info.link;
-        textSpan.classList.add('text');
-        textSpan.appendChild(link);
-
-        listItem.appendChild(iconSpan);
-        listItem.appendChild(textSpan);
-        contactList.appendChild(listItem);
-    });
+    contactList.innerHTML = contactInfoData.map(function (info) {
+        return `
+            <li>
+                <span class="icon"><i class="${info.icon}"></i></span>
+                <span class="text"><a href="${info.link}">${info.text}</a></span>
+            </li>
+        `;
+    }).join('');
 }
 
 document.addEventListener('DOMContentLoaded', generateContactInfo);
@@ -36,36 +26,57 @@ document.addEventListener('DOMContentLoaded', generateContactInfo);
 
 // DATI ISTRUZIONE
 var educationInfoData = [
-    { year: '2020', degree: 'Master Degree in Business Management and Digital Technologies', university: 'UTIU' },
-    { year: '2017', degree: 'Bachelor Degree in Management and Production Engineering', university: 'Politecnico di Milano' }
+    { year: '2020', degree: 'Laurea Magistrale in Business Management e Tecnologie Digitali', university: 'UTIU' },
+    { year: '2017', degree: 'Laurea Triennale in Ingegneria dell\'Informazione, corso Gestionale', university: 'Politecnico di Milano' }
 ];
 
 function generateEducationInfo() {
     var educationList = document.getElementById('educationList');
 
-    educationInfoData.forEach(function (info) {
-        var listItem = document.createElement('li');
-        var yearHeading = document.createElement('h5');
-        var degreeHeading = document.createElement('h4');
-        var universityHeading = document.createElement('h4');
-
-        yearHeading.textContent = info.year;
-        degreeHeading.textContent = info.degree;
-        universityHeading.textContent = info.university;
-
-        listItem.appendChild(yearHeading);
-        listItem.appendChild(degreeHeading);
-        listItem.appendChild(universityHeading);
-        educationList.appendChild(listItem);
-    });
+    educationList.innerHTML = educationInfoData.map(function (info) {
+        return `
+            <li>
+                <h5>${info.year}</h5>
+                <h4>${info.degree}</h4>
+                <h4>${info.university}</h4>
+            </li>
+        `;
+    }).join('');
 }
 
 document.addEventListener('DOMContentLoaded', generateEducationInfo);
 
 
+// SKILLS
+var skillsData = [
+    { name: 'Html', percentage: '100%' },
+    { name: 'css', percentage: '90%' },
+    { name: 'javascript', percentage: '85%' },
+    { name: 'Angular', percentage: '68%' },
+    { name: 'React.js', percentage: '64%' },
+    { name: 'C#', percentage: '50%' }
+];
 
-// AGGIORNAMENTO
+function generateSkills() {
+    var skillsSection = document.querySelector('.about.skills');
 
+    skillsSection.innerHTML = `
+        <h2 class="title2">Skills</h2>
+        ${skillsData.map(skill => `
+            <div class="box">
+                <h4>${skill.name}</h4>
+                <div class="percent">
+                    <div style="width: ${skill.percentage}"></div>
+                </div>
+            </div>
+        `).join('')}
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', generateSkills);
+
+
+// AGGIORNAMENTO DATA ODIERNA
 const today = new Date();
 const year = today.getFullYear();
 const month = new Intl.DateTimeFormat('it', { month: 'long' }).format(today);
